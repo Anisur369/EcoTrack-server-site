822264c71cfcf3a35227fc443b465d69efaf5c1d
Anisur369 <anisurrahman57136@gmail.com>
2025-11-14 19:14:21 +0600

    commit

--- index.js
+++ index.js
@@ -28,11 +28,10 @@ async function run() {
     await client.connect();
     const ecoTrack = client.db("EcoTrack");
     const challengesCollection = ecoTrack.collection("challenges");
-    const productsCollection = ecoTrack.collection("products");
-    const bidsCollection = ecoTrack.collection("bids");
+    const tipsCollection = ecoTrack.collection("tips");
+    const eventsCollection = ecoTrack.collection("events");
 
     // challenges related apis
-
     app.get("/challenges", async (req, res) => {
       const cursor = challengesCollection.find({});
       const result = await cursor.toArray();
@@ -97,156 +96,125 @@ async function run() {
       res.send(result);
     });
 
-    // app.post("/challenges", async (req, res) => {
-    //   const newUser = req.body;
-    //   const email = req.body.email;
-    //   const query = { email: email };
-    //   console.log(query);
-    //   const existingUser = await usersCollection.findOne(query);
-
-    //   if (existingUser) {
-    //     res.send({ message: "user already exists" });
-    //   } else {
-    //     const result = await usersCollection.insertOne(newUser);
-    //     res.send(result);
-    //   }
-    // });
-    // app.delete("/users/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) };
-    //   const result = await usersCollection.deleteOne(query);
-    //   res.send(result);
-    // });
-    // app.put("/users/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const updatedUser = req.body;
-    //   const query = { _id: new ObjectId(id) };
-    //   const options = { upsert: true };
-    //   const updateDoc = {
-    //     $set: {
-    //       name: updatedUser.name,
-    //       email: updatedUser.email,
-    //     },
-    //   };
-    //   const result = await usersCollection.updateOne(query, updateDoc, options);
-    //   res.send(result);
-    // });
-
-    // products related apis
-
-    app.get("/products", async (req, res) => {
-      // const projectFields = { name: 1, price_min: 1 };
-      // const cursor = productsCollection
-      //   .find({})
-      //   .sort({ price_min: 1 })
-      //   .skip(0)
-      //   .limit(3)
-      //   .project(projectFields);
-
-      const email = req.query.email;
-      const query = {};
-      if (email) {
-        query.email = email;
-        console.log(query);
-      }
-      const cursor = productsCollection.find(query);
+    // tips related apis
+    app.get("/tips", async (req, res) => {
+      const cursor = tipsCollection.find({});
       const result = await cursor.toArray();
       res.send(result);
     });
-    app.get("/latest-products", async (req, res) => {
-      const projectFields = { name: 1, image: 1, price_min: 1 };
-      const cursor = productsCollection
-        .find({})
-        .sort({ created_at: -1 })
-        .skip(0)
-        .limit(6);
-      const result = await cursor.toArray();
-      res.send(result);
-    });
-    app.get("/products/:id", async (req, res) => {
+    app.get("/tips/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      console.log(query);
-      const result = await productsCollection.findOne(query);
+      const result = await tipsCollection.findOne(query);
       res.send(result);
     });
-    app.post("/products", async (req, res) => {
-      const newUser = req.body;
-      const result = await productsCollection.insertOne(newUser);
+    app.post("/tips", async (req, res) => {
+      const newTip = req.body;
+      console.log(newTip);
+      const result = await tipsCollection.insertOne(newTip);
       res.send(result);
     });
-    app.delete("/products/:id", async (req, res) => {
+    app.delete("/tips/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
-      const result = await productsCollection.deleteOne(query);
+      const result = await tipsCollection.deleteOne(query);
       res.send(result);
     });
-    app.put("/products/:id", async (req, res) => {
+    app.put("/tips/:id", async (req, res) => {
       const id = req.params.id;
-      const updatedUser = req.body;
-      const query = { _id: new ObjectId(id) };
-      const options = { upsert: true };
-      const updateDoc = {
-        $set: updatedUser,
-      };
-      const result = await productsCollection.updateOne(
-        query,
-        updateDoc,
-        options
-      );
-      res.send(result);
-    });
-    app.patch("/products/:id", async (req, res) => {
-      const id = req.params.id;
-      const updatedUser = req.body;
-      const query = { _id: new ObjectId(id) };
+      const updatedTip = req.body;
+      const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
       const updateDoc = {
         $set: {
-          name: updatedUser.name,
-          email: updatedUser.email,
+          title: updatedTip.title,
+          category: updatedTip.category,
+          metric: updatedTip.metric,
+          image: updatedTip.image,
         },
       };
-      const result = await productsCollection.updateOne(
-        query,
+      const result = await tipsCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
+    app.patch("/tips/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedTip = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          title: updatedTip.title,
+          category: updatedTip.category,
+          metric: updatedTip.metric,
+          image: updatedTip.image,
+        },
+      };
+      const result = await tipsCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
+
+    // events related apis
+    app.get("/events", async (req, res) => {
+      const cursor = eventsCollection.find({});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/events/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await eventsCollection.findOne(query);
+      res.send(result);
+    });
+    app.post("/events", async (req, res) => {
+      const newEvent = req.body;
+      console.log(newEvent);
+      const result = await eventsCollection.insertOne(newEvent);
+      res.send(result);
+    });
+    app.delete("/events/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await eventsCollection.deleteOne(query);
+      res.send(result);
+    });
+    app.put("/events/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedEvent = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          title: updatedEvent.title,
+          category: updatedEvent.category,
+          metric: updatedEvent.metric,
+          image: updatedEvent.image,
+        },
+      };
+      const result = await eventsCollection.updateOne(
+        filter,
         updateDoc,
         options
       );
       res.send(result);
     });
-
-    // bids related apis
-
-    app.get("/bids", async (req, res) => {
-      const email = req.query.email;
-      const query = {};
-      if (email) {
-        query.buyer_email = email;
-      }
-      const cursor = bidsCollection.find(query);
-      const result = await cursor.toArray();
-      res.send(result);
-    });
-    app.post("/bids", async (req, res) => {
-      const newBid = req.body;
-      const result = await bidsCollection.insertOne(newBid);
-      res.send(result);
-    });
-    app.delete("/bids/:id", async (req, res) => {
+    app.patch("/events/:id", async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      const result = await bidsCollection.deleteOne(query);
-      res.send(result);
-    });
-    app.put("/bids/:id", async (req, res) => {
-      const id = req.params.id;
-      const updatedBid = req.body;
-      const query = { _id: new ObjectId(id) };
+      const updatedEvent = req.body;
+      const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
       const updateDoc = {
-        $set: updatedBid,
+        $set: {
+          title: updatedEvent.title,
+          category: updatedEvent.category,
+          metric: updatedEvent.metric,
+          image: updatedEvent.image,
+        },
       };
-      const result = await bidsCollection.updateOne(query, updateDoc, options);
+      const result = await eventsCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
       res.send(result);
     });
 
