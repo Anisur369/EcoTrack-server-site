720211058ba9fede4a9188ffd01ca13477d019f7
Anisur369 <anisurrahman57136@gmail.com>
2025-11-19 13:39:58 +0600

    tips id data api

--- index.js
+++ index.js
@@ -273,7 +273,7 @@ async function run() {
 }
 run().catch(console.dir);
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);
 });
